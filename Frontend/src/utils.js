563f3ddee8d24748f7405dd70dbb20f479739732
parimusha-code <parimusha2005@gmail.com>
2026-03-09 11:21:48 +0530

    connect frontend to backend

--- Frontend/src/utils.js
+++ Frontend/src/utils.js
@@ -1,7 +1,7 @@
 // Replace with your deployed backend URL
-const API_URL = "https://your-backend-url.onrender.com";
+const API_URL = "https://ai-resume-builder-vpjr.onrender.com";
 
 // Example of using it
-fetch(`${API_URL}/resume`)
+fetch(`${API_URL}/api/resumes`)
   .then(res => res.json())
   .then(data => console.log(data));
